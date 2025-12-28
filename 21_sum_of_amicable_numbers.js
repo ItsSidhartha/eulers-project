3a910f70bff488a41c21198d8e9b7bcc133044e4
Sidhartha Maji <sidharthamaji@Sidharthas-MacBook-Pro.local>
2025-12-28 20:54:19 +0530

    added 21st

---
+++ 21_sum_of_amicable_numbers.js
@@ -0,0 +1,34 @@
+const sumOfFactorsOf = (num) => {
+  if (num === 1) return 1;
+  const largestFactor = Math.sqrt(num);
+  let sum = 1;
+  for (let term = 2; term < largestFactor; term++) {
+    if (num % term === 0) {
+      sum = sum + term + (num / term);
+    }
+  }
+  if (largestFactor === Math.floor(largestFactor)) sum += largestFactor;
+  return sum;
+};
+
+const isAmicable = (num) => {
+  const sumOfFactors = sumOfFactorsOf(num);
+  if (num === sumOfFactors) return false;
+  return num === sumOfFactorsOf(sumOfFactors);
+};
+
+const sumOfAmicableNums = (limit) => {
+  let sum = 0;
+  let candidate = 0;
+  while (candidate < limit) {
+    if (isAmicable(candidate)) {
+      const compliment = sumOfFactorsOf(candidate);
+      sum = sum + candidate + compliment;
+      candidate = compliment
+    }
+    candidate++;
+  }
+  return sum;
+};
+
+console.log(sumOfAmicableNums(...Deno.args));
