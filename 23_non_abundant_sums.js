3ab71313cbd2c04dcc24d1d203300c34c3086810
Sidhartha Maji <sidharthamaji@Sidharthas-MacBook-Pro.local>
2026-01-01 15:23:40 +0530

    added sumation and made some abstruction

--- 23_non_abundant_sums.js
+++ 23_non_abundant_sums.js
@@ -1,68 +1,71 @@
-function SumOfDivisors(n) {
+const sumOfDivisors = (num) => {
   let sum = 1;
-  let p = 2;
-  while (p * p <= n && n > 1) {
-    if (n % p === 0) {
-      let j = p * p; // 9
-      n = Math.floor(n / p);
-      while (n % p === 0) {
-        j = j * p;
-        n = Math.floor(n / p);
+  let divisor = 2;
+
+  while (divisor * divisor <= num && num > 1) {
+    if (num % divisor === 0) {
+      let exponent = divisor * divisor;
+      num = Math.floor(num / divisor);
+
+      while (num % divisor === 0) {
+        exponent = exponent * divisor;
+        num = Math.floor(num / divisor);
       }
-      sum = sum * (j - 1); // 26
-      sum = Math.floor(sum / (p - 1)); // 13
+
+      sum = sum * (exponent - 1);
+      sum = Math.floor(sum / (divisor - 1));
     }
-    if (p === 2) p = 3;
-    else p = p + 2;
+
+    divisor = (divisor === 2) ? 3 : divisor + 2;
   }
-  if (n > 1) sum = sum * (n + 1);
+
+  if (num > 1) sum = sum * (num + 1);
   return sum;
-}
+};
 
 const sumOfFactorsOf = (num) => {
-  // if (num === 1) return 1;
-  // const largestFactor = Math.sqrt(num);
-  // let sum = 1;
-  // for (let term = 2; term < largestFactor; term++) {
-  //   if (num % term === 0) {
-  //     sum = sum + term + (num / term);
-  //   }
-  // }
-  // if (largestFactor === Math.floor(largestFactor)) sum += largestFactor;
-  // return sum;
-
-  return SumOfDivisors(num) - num;
+  return sumOfDivisors(num) - num;
 };
 
 const isAbundant = (candidate) => sumOfFactorsOf(candidate) > candidate;
 
 const allAbundants = (limit) => {
-  const abundants = new Set();
+  const abundants = [];
 
   for (let candidate = 1; candidate < limit; candidate++) {
     if (isAbundant(candidate)) {
-      console.log({candidate});
-      abundants.add(candidate);
+      abundants.push(candidate);
     }
   }
+
   return abundants;
 };
 
-const isAbundantSum = (num, abundants) => {
-  for (const element of abundants) {
-    const compliment = num - element;
-    if (abundants.has(compliment)) return true;
+const markAbundantSums = (abundants, limit) => {
+  const abundantMarkedArray = Array(limit + 1).fill(false);
+
+  for (let i = 0; i < abundants.length; i++) {
+    for (let j = i; j < abundants.length; j++) {
+      const sum = abundants[i] + abundants[j];
+      if (sum > limit) break;
+      abundantMarkedArray[sum] = true;
+    }
   }
-  return false;
+
+  return abundantMarkedArray;
 };
+
+const sumOfNonAbundants = (markedArray) =>
+  markedArray.reduce((sum, ele, i) => {
+    if (!ele) sum = sum + i;
+    return sum;
+  }, 0);
 
 const countNonAbundantSums = (limit) => {
   const abundants = allAbundants(limit);
-  let count = 0;
-  for (let term = 0; term < limit; term++) {
-    if (!isAbundantSum(term, abundants)) count++;
-  }
-  return count;
+  const markedAbundandSums = markAbundantSums(abundants, limit);
+
+  return sumOfNonAbundants(markedAbundandSums);
 };
 
 console.log(countNonAbundantSums(28123));
